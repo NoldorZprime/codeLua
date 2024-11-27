24ec8987867cc3d52fa01fb9e19a0366f179276b
alexroel <alexroel@example.com>
2024-11-27 14:17:40 -0500

    codelua-v0.3

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -1,12 +1,10 @@
 export const sidebar = [
     {
         label: "Primeros pasos con Lua",
-        translations: { 'en': 'Getting started with Lua' },
         autogenerate: { directory: "01-get-started-with-lua" },
     },
     {
         label: "Usu de variables y datos",
-        translations: { 'en': 'Variables and data' },
         autogenerate: { directory: "02-variables-data" },
     },
 ];
